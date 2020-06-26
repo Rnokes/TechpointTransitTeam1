3cbb7782a604b6d41c0ceaac1edf24c2c8ab733d
Rnokes <rnokes@purdue.edu>
2020-06-26 11:27:58 -0400

    Small UI bugfix.

--- app/src/main/java/com/example/transitapp/ui/dashboard/DashboardViewModel.java
+++ app/src/main/java/com/example/transitapp/ui/dashboard/DashboardViewModel.java
@@ -10,7 +10,7 @@ public class DashboardViewModel extends ViewModel {
 
     public DashboardViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
+        mText.setValue("Hello world\nPossible Login Page");
     }
 
     public LiveData<String> getText() {
