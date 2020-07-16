56f38931fb3950c330f0d76f140c797cc9f76c05
dfirestorm <haudj@rose-hulman.edu>
2020-07-16 14:11:47 -0500

    Linking screens

--- app/src/main/java/com/example/transync/MyAlertsScreen.java
+++ app/src/main/java/com/example/transync/MyAlertsScreen.java
@@ -4,30 +4,19 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 
-public class MyRoutesScreen extends Activity {
-
+public class MyAlertsScreen extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.my_routes_screen);
+        setContentView(R.layout.my_alerts_screen);
 
         ImageButton menu = findViewById(R.id.menubutton);
         menu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(MyRoutesScreen.this, HomeScreen.class);
-                startActivity(i);
-            }
-        });
-
-        Button myAlerts = findViewById(R.id.myAlerts);
-        myAlerts.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(MyRoutesScreen.this, MyAlertsScreen.class);
+                Intent i = new Intent(MyAlertsScreen.this, HomeScreen.class);
                 startActivity(i);
             }
         });
