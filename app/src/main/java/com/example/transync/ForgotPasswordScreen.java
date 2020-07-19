72ff65478d527dc8d4c7a8e23da3184974f71e12
dfirestorm <haudj@rose-hulman.edu>
2020-07-19 14:35:55 -0500

    added Forgot Password screen

---
+++ app/src/main/java/com/example/transync/ForgotPasswordScreen.java
@@ -0,0 +1,17 @@
+
+package com.example.transync;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+public class ForgotPasswordScreen extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.forgot_password_screen);
+    }
+}
